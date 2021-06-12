e55319efa93c9af4c3b9acffa74ca4794808dee0
kdw <zkdlwu94@naver.com>
2021-06-12 11:32:01 +0900

    first

---
+++ server.js
@@ -0,0 +1,70 @@
+const WebSocket = require("ws");
+const logger = require("./lib/logger");
+
+const streamName = "btcbusd@kline_15m";
+
+const ws = new WebSocket(`wss://stream.binance.com:9443/ws/${streamName}`);
+
+ws.onopen = (e) => {
+  logger.info(`ws connected ${e}` );
+};
+
+ws.on("pong", () => {
+  logger.debug("receieved pong from server");
+});
+ws.on("ping", () => {
+  logger.debug("==========receieved ping from server");
+  ws.pong();
+});
+
+ws.onclose = () => {
+  logger.warn("ws closed");
+};
+
+ws.onerror = (err) => {
+  logger.warn("ws error", err);
+};
+ws.onmessage = (msg) => {
+    try {
+      const message = JSON.parse(msg.data);
+      logger.info(message.k.t);
+      console.log(message.k.t)
+      /*
+      if (message.e) {
+        if (this._handlers.has(message.e)) {
+          this._handlers.get(message.e).forEach((cb) => {
+            cb(message);
+          });
+        } else {
+          logger.warn('Unprocessed method', message);
+        }
+      } else {
+        logger.warn('Unprocessed method', message);
+      }
+    } catch (e) {
+      logger.warn('Parse message failed', e);
+    }
+    */
+    }catch(e){
+        logger.warn('Parse message failed', e);
+    }
+  };
+setInterval(() => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.ping();
+      logger.debug("ping server");
+    }
+  }, 5000);
+
+let lastCurrent = +new Date();
+/*
+ws.setHandler("depthUpdate", (params) => {
+  const current = +new Date();
+  const EvsNow = current - params.E;
+  const fromLastEvent = current - lastCurrent;
+  console.log(
+    `[Spot ${streamName}] current:${current} fromLastEvent: ${fromLastEvent} delta EvsNow: ${EvsNow}`
+  );
+  lastCurrent = current;
+});
+*/
