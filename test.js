8cade68678695c1d6ce45b75743b7509bbae270e
dongwan <zkdlwu94@naver.com>
2021-06-15 15:47:31 +0900

    shell js update

---
+++ test.js
@@ -0,0 +1,20 @@
+const WebSocket = require("ws");
+const logger = require("./lib/logger");
+const axios = require("axios");
+const puppeteer = require("puppeteer");
+const funcmessage = require("./lib/message");
+
+const streamName = "btcbusd@kline_1m";
+const streamName2 = "btcbusd@kline_15m";
+
+const AVERAGECOUNT = 16;
+const ONEMINRATIO = 9;
+const FIFTMINRATIO = 5;
+let html = "";
+let prevPageData;
+
+
+funcmessage.sendToMessage( {
+    title:'시범 데이터 발송11',
+    body:'바디 데이터 전송'
+});
